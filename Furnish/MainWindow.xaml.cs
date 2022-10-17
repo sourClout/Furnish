873e3fe8e0239e7664792d1e148590423e0bc3e6
shift1018 <105242991+shift1018@users.noreply.github.com>
2022-10-17 09:13:49 -0400

    login

--- Furnish/MainWindow.xaml.cs
+++ Furnish/MainWindow.xaml.cs
@@ -15,9 +15,7 @@ using System.Windows.Shapes;
 
 namespace Furnish
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
         public MainWindow()
@@ -25,19 +23,39 @@ namespace Furnish
             InitializeComponent();
         }
 
-        private void BtnOrders_Click(object sender, RoutedEventArgs e)
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
         {
-
+            if(e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
         }
 
-        private void BtnProducts_Click(object sender, RoutedEventArgs e)
+
+        private bool IsMaximized = false;
+
+        private void Border_MouseLeftButtonDown (object sender, MouseButtonEventArgs e)
         {
+            if (e.ClickCount == 2)
+            {
+                if (IsMaximized)
+                {
+                    this.WindowState = WindowState.Normal;
+                    this.Width = 1080;
+                    this.Height = 720;
 
-        }
+                    IsMaximized = false;
 
-        private void BtnCustomers_Click(object sender, RoutedEventArgs e)
-        {
+                }
+                else
+                {
+                    this.WindowState = WindowState.Maximized;
+                    IsMaximized=true;
+                }
 
+
+
+            }
         }
     }
 }
